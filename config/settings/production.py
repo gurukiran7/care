8e737ef858d4eb7e4f092de0c3414ec4a701a8bb
Gurukiran <146521148+gurukiran7@users.noreply.github.com>
2025-09-03 00:19:53 +0530

    Update production settings for debugging

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,8 +1,11 @@
 from .deployment import *  # noqa
 
-REST_FRAMEWORK["DEFAULT_AUTHENTICATION_CLASSES"] = (  # noqa F405
-    "config.authentication.CustomJWTAuthentication",
-)
+# REST_FRAMEWORK["DEFAULT_AUTHENTICATION_CLASSES"] = (  # noqa F405
+#     "config.authentication.CustomJWTAuthentication",
+# )
+DEBUG = True
+ALLOWED_HOSTS = ["*"]
+
 
 IS_PRODUCTION = True
 USE_SMS = env.bool("USE_SMS", default=True)  # noqa F405
